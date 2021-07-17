1dc519d588c34af982f83deeda300b0724896e9f
anemology <anemology@users.noreply.github.com>
2021-07-17 15:30:51 +0800

    refactor

--- lambda_function.py
+++ lambda_function.py
@@ -15,7 +15,8 @@ def lambda_handler(event, context):
     # EventBridge (CloudWatch Events)
     # 定期執行的排程
     if event.get("source") == "aws.events":
-        get_net_value(bot)
+        fund_data = FundData(URL)
+        send_message(bot, fund_data.get_net_value())
         return
 
     # 判斷是 post 以及有 body 才往下走, 代表是從 telegram 發來的 webhook
@@ -33,72 +34,57 @@ def lambda_handler(event, context):
     if text == "/start":
         response_text = "Started!"
         bot.sendMessage(chat_id=chat_id, text=response_text)
+        return
 
     if text.startswith("/url"):
-        get_url(bot)
-
+        send_message(bot, URL)
+        return
+    
+    fund_data = FundData(URL)
     if text.startswith("/now"):
-        get_net_value(bot)
+        send_message(bot, fund_data.get_net_value())
+        return
 
     if text.startswith("/high"):
-        get_highest_value(bot)
+        send_message(bot, fund_data.get_highest_value())
+        return
 
     if text.startswith("/low"):
-        get_lowest_value(bot)
+        send_message(bot, fund_data.get_lowest_value())
+        return
 
 
-def get_fund_data():
-    """取得 MoneyDJ 基金資料"""
-    response = requests.get(URL)
-    soup = BeautifulSoup(response.text, "html.parser")
+class FundData():
+    def __init__(self, url):
+        """取得 MoneyDJ 基金資料"""
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
 
-    overview_table = soup.select_one("#article > form > table.t01 > tr:nth-child(2)")
-    # 淨值日期
-    net_value_date = overview_table.select_one("td:nth-child(1)").get_text()
-    # 最新淨值
-    net_value = overview_table.select_one("td:nth-child(2)").get_text()
-    # 每日變化
-    net_value_change = overview_table.select_one("td:nth-child(3)").get_text()
-    # 最高淨值(年)
-    net_value_highest = overview_table.select_one("td:nth-child(4)").get_text()
-    # 最低淨值(年)
-    net_value_lowest = overview_table.select_one("td:nth-child(5)").get_text()
+        overview_table = soup.select_one("#article > form > table.t01 > tr:nth-child(2)")
+        # 淨值日期
+        self.net_value_date = overview_table.select_one("td:nth-child(1)").get_text()
+        # 最新淨值
+        self.net_value = overview_table.select_one("td:nth-child(2)").get_text()
+        # 每日變化
+        self.net_value_change = overview_table.select_one("td:nth-child(3)").get_text()
+        # 最高淨值(年)
+        self.net_value_highest = overview_table.select_one("td:nth-child(4)").get_text()
+        # 最低淨值(年)
+        self.net_value_lowest = overview_table.select_one("td:nth-child(5)").get_text()
 
-    return {
-        "net_value_date": net_value_date,
-        "net_value": net_value,
-        "net_value_change": net_value_change,
-        "net_value_highest": net_value_highest,
-        "net_value_lowest": net_value_lowest,
-    }
+    def get_net_value(self):
+        """現在淨值"""
+        return f"淨值日期:{self.net_value_date}, 最新淨值:{self.net_value}, 每日變化:{self.net_value_change}"
 
+    def get_highest_value(self):
+        """最高淨值(年)"""
+        return f"最高淨值(年):{self.net_value_highest}"
 
-def get_url(bot):
-    """回傳抓取的 URL"""
-    bot.sendMessage(chat_id=MY_CHAT_ID, text=URL)
-
-
-def get_net_value(bot):
-    """回傳現在淨值"""
-    fund_data = get_fund_data()
-    net_value_date = fund_data.get("net_value_date")
-    net_value = fund_data.get("net_value")
-    net_value_change = fund_data.get("net_value_change")
-    text = f"淨值日期:{net_value_date}, 最新淨值:{net_value}, 每日變化:{net_value_change}"
-    bot.sendMessage(chat_id=MY_CHAT_ID, text=text)
+    def get_lowest_value(self):
+        """最低淨值(年)"""
+        return f"最低淨值(年):{self.net_value_lowest}"
 
 
-def get_highest_value(bot):
-    """回傳最高淨值(年)"""
-    fund_data = get_fund_data()
-    net_value_highest = fund_data.get("net_value_highest")
-    text = f"最高淨值(年):{net_value_highest}"
-    bot.sendMessage(chat_id=MY_CHAT_ID, text=text)
-
-
-def get_lowest_value(bot):
-    """回傳最低淨值(年)"""
-    fund_data = get_fund_data()
-    net_value_lowest = fund_data.get("net_value_lowest")
-    text = f"最低淨值(年):{net_value_lowest}"
+def send_message(bot, text):
+    """回傳訊息"""
     bot.sendMessage(chat_id=MY_CHAT_ID, text=text)
